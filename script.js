343142498427a481edae31b61f3b737f7c3ad6dd
Eduardo Rivera <lic.riveraeduard@gmail.com>
2022-09-26 10:53:18 -0500

    feat: uso de script externo

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Sigo sin entender Github");
+        document.getElementById("generation").style.color = "grey";
+      function colorTitulo(){
+        console.log("Pulso el botón de Generation");
+        document.getElementById("generation").style.color = "blue";
+      }

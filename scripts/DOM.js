da8cd093b6a77f92a89183c653096f0a31dc5c30
Eduardo Rivera <lic.riveraeduard@gmail.com>
2022-10-03 17:16:03 -0500

    feat: se agregan arhvivos DOM

---
+++ scripts/DOM.js
@@ -0,0 +1,43 @@
+console.log("Sesión JS06 DOM");
+
+//encontrarPorID();
+//encontrarPorTagName();
+//encontrarPorClase();
+encontrarPorSelector();
+
+function encontrarPorID(){
+    //Obtenemos el objeto a través de su ID 
+    const element = document.getElementById("titulo.h1");
+    console.log("Datos del elemento: " + element.innerHTML);
+
+    //Cambiamos los datos en el objeto
+    element.innerHTML= `El <em class="color-em">texto</em> es: " + ${element.innerHTML}`;
+    }
+
+    function encontrarPorTagName(){
+        //Obtenemos los objetos que cumplan con el tag indicado
+        //getElementsByTagName() entrega una colección con los objetos encontrados
+        let elements = document.getElementsByTagName("p");
+        console.log("Elementos encontrados: " + elements.length);
+
+        //Obtenemos el elemento del índice 1;
+        let parrafo = elements [1].innerHTML;
+        console.log("Contenido en p[1]: " + parrafo);
+        console.log("Contenido en P[1] usando innerText: " + elements[1].innerText );
+        //innerHTML nos entrega el objeto con sus elementos anidados.
+        //innerText nos entrega el texto del objeto, sin elementos anidados y sin dobles espacios.
+    }
+
+    function encontrarPorClase(){
+        const elements = document.getElementsByClassName("color");
+        console.log("Elementos encontrados: " + elements.length);
+
+        elements[0].innerHTML = "Reacciones Pro";
+    }
+
+    function encontrarPorSelector(){
+        const elements = document.querySelectorAll("h3>.color");
+        console.log("Elementos encontrados: " + elements.length);
+
+        elements[0].innerHTML = "ganas de comprar ";
+    }
